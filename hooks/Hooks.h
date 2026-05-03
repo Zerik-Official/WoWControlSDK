1c84134578479a61432c6c3d65214a7418096147
Zerik-Official <173982082+Zerik-Official@users.noreply.github.com>
2026-05-03 18:11:38 -0500

    refactor(hooks): Extract hooks into dedicated module

---
+++ hooks/Hooks.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "../GameClient.h"
+
+namespace Hooks {
+
+using DummyCallback_t = void(*)();
+
+namespace FrameScript {
+using TokenGuidGetter  = guid_t();
+using TokenNGuidGetter = guid_t(int);
+using TokenIdGetter    = bool(guid_t);
+using TokenIdNGetter   = int(guid_t);
+
+void registerToken(const char* token, TokenGuidGetter* getGuid, TokenIdGetter* getId);
+void registerToken(const char* token, TokenNGuidGetter* getGuid, TokenIdNGetter* getId);
+void registerOnUpdate(DummyCallback_t func);
+}
+
+namespace FrameXML {
+void registerEvent(const char* str);
+void registerCVar(Console::CVar** dst, const char* str, const char* desc, Console::CVarFlags flags, const char* initialValue, Console::CVar::Handler_t func);
+void registerLuaLib(lua_CFunction func);
+}
+
+namespace GlueXML {
+void registerPostLoad(DummyCallback_t func);
+void registerCharEnum(DummyCallback_t func);
+}
+
+void initialize();
+
+}
