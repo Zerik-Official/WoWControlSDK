1fdcd3013b96facba9f67facf3fcaf40b6df679d
Zerik-Official <173982082+Zerik-Official@users.noreply.github.com>
2026-05-05 19:21:16 -0500

    feat(player): extend PlayerState::Info with stats, position, flags and metadata

--- core/PlayerState.h
+++ core/PlayerState.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <string>
+#include <cstdint>
 
 namespace PlayerState {
 
@@ -7,14 +7,32 @@ struct Info {
     float    x;
     float    y;
     float    z;
+    float    rotation;
     int      health;
     int      healthMax;
     int      mana;
     int      manaMax;
+    int      rage;
+    int      rageMax;
+    int      energy;
+    int      energyMax;
+    int      runicPower;
+    int      runicPowerMax;
     int      level;
     int      race;
     int      class_;
     int      gender;
+    int      powerType;
+    int      xp;
+    int      xpMax;
+    int      mapId;
+    int      zoneId;
+    int      tick;
+    bool     isIngame;
+    bool     isWorld;
+    char     name[32];
+    bool isLoading;
+    bool isReady;
 };
 
 bool  available();
