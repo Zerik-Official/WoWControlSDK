6f7c5a76f38fc9d2c72bfe4c520ca63cf63e6c56
Zerik-Official <173982082+Zerik-Official@users.noreply.github.com>
2026-05-16 10:14:02 -0500

    refactor(core/map): add MapReader for position and zone data

---
+++ core/map/MapReader.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <cstdint>
+
+namespace MapReader {
+
+struct Info {
+    float    x;
+    float    y;
+    float    z;
+    float    rotation;
+    int      mapId;
+    int      zoneId;
+    int      continentId;
+    int      luaMapId;
+    char     zoneName[128];
+    char     subZoneName[128];
+};
+
+Info read(uintptr_t objectBase);
+
+}
