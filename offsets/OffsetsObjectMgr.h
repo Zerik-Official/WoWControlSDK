809fb714ca695d475691a9e161541d4ced6b9f40
Zerik-Official <173982082+Zerik-Official@users.noreply.github.com>
2026-05-17 10:39:24 -0500

    chore(offsets): add object manager unit type mask

--- offsets/OffsetsObjectMgr.h
+++ offsets/OffsetsObjectMgr.h
@@ -11,6 +11,7 @@ namespace Offsets
         constexpr uintptr_t LOCAL_GUID_OFFSET        = 0xC0;
 
         constexpr uintptr_t FIND_OBJ_BY_GUID         = 0x004D4DB0;
+        constexpr uint32_t OBJ_TYPE_MASK_UNIT        = 0x8 | 0x10;
 
         constexpr uintptr_t OBJECT_POS_X             = 0x9B8;
         constexpr uintptr_t OBJECT_POS_Y             = 0x9BC;
