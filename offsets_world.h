01348ee633089a0906b69cab4ade9ac8a692fb5e
Zerik-Official <173982082+Zerik-Official@users.noreply.github.com>
2026-05-17 10:42:52 -0500

    chore(offsets): remove unused world offsets header

--- offsets_world.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "offsets/OffsetsLua.h"
-#include "offsets/OffsetsObjectMgr.h"
-#include "offsets/OffsetsUnit.h"
-#include "offsets/OffsetsPlayer.h"
-#include "offsets/OffsetsGroup.h"
-#include "offsets/OffsetsMap.h"
