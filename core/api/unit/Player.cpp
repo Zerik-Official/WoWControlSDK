3de2e91976669af929c557a6ccd7ce0b7082e153
Zerik-Official <173982082+Zerik-Official@users.noreply.github.com>
2026-05-17 08:08:06 -0500

    feat(player): add local player accessors and xp support

---
+++ core/api/unit/Player.cpp
@@ -0,0 +1,39 @@
+#include "Player.h"
+#include "memory/MemReader.h"
+#include "object/ObjectManager.h"
+#include "OffsetsPlayer.h"
+#include "OffsetsObjectMgr.h"
+
+Player Player::local()
+{
+    WoWGUID guid = WoW::GetLocalGUID();
+
+    if (!guid.isValid())
+        return Player(0);
+
+    return Player(WoW::GetObjectByGUID(guid));
+}
+
+int Player::getXP() const
+{
+    if (!isValid())
+        return 0;
+
+    return Memory::safeRead<int>(m_descBase + Offsets::ObjectMgr::OBJ_DESC_END + Offsets::Player::Desc::XP);
+}
+
+int Player::getMaxXP() const
+{
+    if (!isValid())
+        return 0;
+
+    return Memory::safeRead<int>(m_descBase + Offsets::ObjectMgr::OBJ_DESC_END + Offsets::Player::Desc::NEXT_LEVEL_XP);
+}
+
+float Player::getRotation() const
+{
+    if (!isValid())
+        return 0.0f;
+
+    return Memory::safeRead<float>(m_base + Offsets::Player::ROT_OFFSET);
+}
