375bd39b1bb5e9cb12068a4d8378e7255863c018
Zerik-Official <173982082+Zerik-Official@users.noreply.github.com>
2026-05-22 18:07:51 -0500

    refactor(client): move GameClient.h to client/ as module aggregator

---
+++ client/GameClient.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "types/Types.h"
+#include "globals/ClientGlobals.h"
+#include "strings/RCString.h"
+#include "login/LoginAPI.h"
+#include "console/ConsoleAPI.h"
+#include "lua/LuaAPI.h"
+#include "lua/LuaTypes.h"
+#include "lua/LuaDefines.h"
+#include "lua/LuaRegister.h"
+#include "game/ErrorAPI.h"
+#include "utils/CString.h"
+
+namespace LoginUI = WoW::Login;
+namespace Console = WoW::Console;
+namespace NetClient = WoW::NetClient;
+namespace CGame = WoW::Game;
+namespace RCString = WoW::RCString;
+
+inline bool IsInWorld() { return WoW::Client::IsInWorld(); }
+
+#define lua_pushcfunction(L, f) lua_pushcclosure(L, f, 0)
