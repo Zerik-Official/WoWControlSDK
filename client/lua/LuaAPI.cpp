c8271871eb785f91e582f27270b2493419a7a1a0
Zerik-Official <173982082+Zerik-Official@users.noreply.github.com>
2026-06-22 14:06:59 -0500

    refactor(client): replace hardcoded addresses with Offsets constants

--- client/lua/LuaAPI.cpp
+++ client/lua/LuaAPI.cpp
@@ -1,21 +1,22 @@
 #include "LuaAPI.h"
+#include "offsets/OffsetsLua.h"
 
 lua_State* GetLuaState()
 {
-    return ((decltype(&GetLuaState))0x00817DB0)();
+    return ((decltype(&GetLuaState))Offsets::Lua::LUA_GETSTATE)();
 }
 
 void lua_settop(lua_State* L, int idx)
 {
-    return ((decltype(&lua_settop))0x0084DBF0)(L, idx);
+    return ((decltype(&lua_settop))Offsets::Lua::LUA_SETTOP)(L, idx);
 }
 
 void lua_pushcclosure(lua_State* L, lua_CFunction f, int c)
 {
-    return ((decltype(&lua_pushcclosure))0x0084E400)(L, f, c);
+    return ((decltype(&lua_pushcclosure))Offsets::Lua::LUA_PUSHCCLOSURE)(L, f, c);
 }
 
 void lua_setfield(lua_State* L, int idx, const char* s)
 {
-    return ((decltype(&lua_setfield))0x0084E900)(L, idx, s);
+    return ((decltype(&lua_setfield))Offsets::Lua::LUA_SETFIELD)(L, idx, s);
 }
