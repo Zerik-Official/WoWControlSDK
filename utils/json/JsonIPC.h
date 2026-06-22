232a75bd7d29d00e676bedcb690da8245951d6b4
Zerik-Official <173982082+Zerik-Official@users.noreply.github.com>
2026-06-22 00:01:48 -0500

    chore(utils): remove unused serializeGameState and CharacterInfo struct

--- utils/json/JsonIPC.h
+++ utils/json/JsonIPC.h
@@ -6,25 +6,6 @@
 namespace SDK::JsonIPC
 {
 
-struct CharacterInfo
-{
-    const char* name;
-    int level;
-    int class_;
-    int race;
-    int gender;
-    int map;
-    int zone;
-};
-
-std::string serializeGameState(
-    const char* screen,
-    bool inWorld,
-    const char* realmList,
-    const CharacterInfo* characters,
-    int characterCount
-);
-
 std::string serializeCommandResponse(bool ok, const char* error = nullptr);
 std::string serializeCommandError(const char* error);
 
